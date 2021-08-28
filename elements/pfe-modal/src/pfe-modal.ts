4ab4d1edb354fae2a6d6b1654a88af2fbcdc79dd
heyMP <mgp140@gmail.com>
2021-08-28 10:46:28 -0400

    fix: cleanup event listeners

--- elements/pfe-modal/src/pfe-modal.ts
+++ elements/pfe-modal/src/pfe-modal.ts
@@ -31,22 +31,11 @@ export class PfeModal extends PFElement {
     this._outer = this.shadowRoot.querySelector(`.${this.constructor.tag}__outer`);
     // event listeners
     this.addEventListener("keydown", this._keydownHandler.bind(this));
-    this._modalCloseButton.addEventListener("keydown", this._keydownHandler.bind(this));
-    this._modalCloseButton.addEventListener("click", this.close.bind(this));
-    this._overlay.addEventListener("click", this.close.bind(this));
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
-
     this.removeEventListener("keydown", this._keydownHandler.bind(this));
-    this._modalCloseButton.removeEventListener("click", this.close.bind(this));
-    this._modalCloseButton.removeEventListener("click", this.close.bind(this));
-    this._overlay.removeEventListener("click", this.close.bind(this));
-
-    if (this.trigger) {
-      this.trigger.removeEventListener("click", this.open.bind(this));
-    }
   }
 
   _keydownHandler(event) {
@@ -142,14 +131,14 @@ export class PfeModal extends PFElement {
         <slot name="pfe-modal--trigger"></slot>
       </span>
       <section class="pfe-modal__outer" hidden>
-        <div class="pfe-modal__overlay" @click=${this.toggle}></div>
+        <div class="pfe-modal__overlay" @click=${this.close}></div>
         <div class="pfe-modal__window" tabindex="0" role="dialog" hidden>
           <div class="pfe-modal__container">
             <div class="pfe-modal__content">
               <slot name="pfe-modal--header"></slot>
               <slot></slot>
             </div>
-            <button class="pfe-modal__close" aria-label="Close dialog">
+            <button class="pfe-modal__close" @click=${this.close} @keydown=${this._keydownHandler} aria-label="Close dialog">
               <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="-11 11 22 23">
                 <path d="M30 16.669v-1.331c0-0.363-0.131-0.675-0.394-0.938s-0.575-0.394-0.938-0.394h-10.669v-10.65c0-0.362-0.131-0.675-0.394-0.938s-0.575-0.394-0.938-0.394h-1.331c-0.363 0-0.675 0.131-0.938 0.394s-0.394 0.575-0.394 0.938v10.644h-10.675c-0.362 0-0.675 0.131-0.938 0.394s-0.394 0.575-0.394 0.938v1.331c0 0.363 0.131 0.675 0.394 0.938s0.575 0.394 0.938 0.394h10.669v10.644c0 0.363 0.131 0.675 0.394 0.938 0.262 0.262 0.575 0.394 0.938 0.394h1.331c0.363 0 0.675-0.131 0.938-0.394s0.394-0.575 0.394-0.938v-10.637h10.669c0.363 0 0.675-0.131 0.938-0.394 0.269-0.262 0.4-0.575 0.4-0.938z" transform="rotate(45)"/>
               </svg>
